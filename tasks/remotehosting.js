69be33111a633f14e54c555eb7ff12d40f2cc0e5
Jeroen Verfallie <verfalliejeroen@gmail.com>
2015-03-17 11:36:51 +0100

    Updated the default rsync exclude.

--- tasks/remotehosting.js
+++ tasks/remotehosting.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
 
 
 
-        var rsyncDefaultExclude = ["cache/","app/storage/***/*.*","storage/***/*.*"];
+        var rsyncDefaultExclude = ["cache/","storage/*.*","storage/***/*.*"];
         var rsyncExclude = jsonConfig.rsync && jsonConfig.rsync.exclude || [];
         rsyncExclude = rsyncExclude.concat(rsyncDefaultExclude);
 
